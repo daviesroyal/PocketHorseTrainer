5606c15e3f678ef02751bbe1668dcb2f953812b8
Rhiannon Davies <ro.morningstar@gmail.com>
2021-03-20 20:08:10 -0500

    Implemented user registration on the frontend.

---
+++ PocketHorseTrainer/PocketHorseTrainer/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PocketHorseTrainer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PocketHorseTrainer.ViewModels
+{
+    class RegisterViewModel
+    {
+        private readonly ApiConnector _apiConnector = new ApiConnector();
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DOB { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        public string Message { get; set; }
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var isRegistered = await _apiConnector.RegisterUserAsync
+                        (FirstName, LastName, UserName, Email, Phone, DOB, Password, ConfirmPassword);
+
+                    if (isRegistered)
+                    {
+                        Message = "Success :)";
+                    }
+                    else
+                    {
+                        Message = "Please try again :(";
+                    }
+                });
+            }
+        }
+    }
+}
